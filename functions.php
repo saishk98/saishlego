0590d0818c0300a2b6a5a47613cbf9dd2d9de52c
saishk98 <89117911+saishk98@users.noreply.github.com>
2021-09-28 11:58:10 +0800

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/** 
+ * For more info: https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ */			
+	
+// Theme support options
+require_once(get_template_directory().'/functions/theme-support.php'); 
+
+// WP Head and other cleanup functions
+require_once(get_template_directory().'/functions/cleanup.php'); 
+
+// Register scripts and stylesheets
+require_once(get_template_directory().'/functions/enqueue-scripts.php'); 
+
+// Register custom menus and menu walkers
+require_once(get_template_directory().'/functions/menu.php'); 
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lego</title>
+</head>
+
+<body>
+<?php get_header(); ?>
+
+<?php
+
+get_template_part( 'template-parts/page/content-front-page', 'none' );
+
+?>
+
+<?php get_footer(); ?>
+
+</body>
